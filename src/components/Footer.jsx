c5e134d59241bc4138e32790e3ab067c3c4e9af0
LucianoLlanos <lucianollanos90@gmail.com>
2025-04-21 09:41:44 -0300

    se crea el footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut ex quos sapiente molestiae fuga ipsa mollitia dolorum perferendis reiciendis cumque quidem saepe quibusdam, beatae impedit voluptatem doloribus, officiis qui magnam?
+    Ipsa magnam sequi commodi quibusdam inventore incidunt saepe eius, asperiores, nesciunt adipisci molestias voluptatum facere quasi maiores odio perspiciatis corporis, a ea illum neque dolor. Libero exercitationem vero distinctio iste!
+    Autem qui earum et quia, inventore perferendis, debitis aliquam molestias harum quibusdam explicabo assumenda ab, enim possimus eveniet libero nesciunt nisi similique natus facere voluptas fugiat nam. Quidem, provident necessitatibus.
+    Saepe qui ad veritatis, mollitia voluptates non reprehenderit at exercitationem corporis porro ducimus, perspiciatis magni placeat animi doloribus temporibus maiores magnam cumque aliquam atque dicta. Doloremque illo rerum beatae quasi?
+    Rem odit ducimus cupiditate? Nisi eaque ex enim ratione eum sunt ipsam sequi officiis, cumque atque unde molestias recusandae mollitia necessitatibus? Consequuntur eligendi suscipit nemo quaerat earum, ut laudantium dolores!
+    Sequi debitis deleniti porro voluptatibus dolorem corrupti accusantium, vel incidunt id? Incidunt eum, doloremque labore delectus iste tempore minus ex ullam maiores maxime minima laudantium consequatur dolore cum aperiam laborum.
+    Distinctio repudiandae ad error nam tempora iure, consequuntur quasi magnam possimus molestiae id repellat modi unde non alias, cupiditate aspernatur ex ut blanditiis culpa commodi illum dolorem necessitatibus officia! Suscipit!
+    Est facilis dolorum libero autem deleniti? Quasi tenetur praesentium at optio quidem rem cum, blanditiis voluptatum esse vel? Et a aspernatur, consequatur illo voluptatem voluptas quia velit repudiandae est officia.</p></div>
+  )
+}
+
+export default Footer
