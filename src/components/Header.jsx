27250185ecab539710ae6743c7bd3d9b24b2549f
JuanmaAgostino <juanmanuelagostino@gmail.com>
2025-04-21 10:56:21 -0300

    creamos header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div><h1>Pruebal</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi error libero assumenda iusto voluptas facilis voluptates a sequi ipsum voluptatum ex culpa, perferendis maxime incidunt exercitationem optio nesciunt laudantium! Aut?
+                Tempore placeat possimus, tenetur ducimus fugiat natus? Quidem dignissimos, distinctio repellendus earum cumque possimus, ut deserunt accusamus, quis veritatis vel! Fuga modi repellendus accusantium itaque, nam cum dignissimos reprehenderit cupiditate?</p></div>
+    )
+}
+
+export default Header
